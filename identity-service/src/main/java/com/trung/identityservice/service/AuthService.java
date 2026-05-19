4856d64fa1a877b7635f237d2f678c67e8fccf30
trung1609 <vmt162005@gmail.com>
2026-05-19 21:08:26 +0700

    ex3

--- identity-service/src/main/java/com/trung/identityservice/service/AuthService.java
+++ identity-service/src/main/java/com/trung/identityservice/service/AuthService.java
@@ -1,10 +1,13 @@
 package com.trung.identityservice.service;
 
 import com.trung.identityservice.config.JwtUtil;
+import com.trung.identityservice.dto.FormLogin;
 import com.trung.identityservice.dto.FormRegister;
+import com.trung.identityservice.dto.JwtResponse;
 import com.trung.identityservice.entity.Users;
 import com.trung.identityservice.repository.AuthRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +18,8 @@ public class AuthService {
     private final PasswordEncoder passwordEncoder;
     private final JwtUtil jwtUtil;
 
+    @Value("${jwt.expiration}")
+    private long expirationTime;
     public String register(FormRegister request){
         Users users = new Users();
         users.setUsername(request.getUsername());
@@ -27,5 +32,18 @@ public class AuthService {
     public String testToken(String username){
         Users users = authRepository.findByUsername(username).orElseThrow(() -> new RuntimeException("User not found"));
         return "Token: " + jwtUtil.generateToken(users);
+    }
+
+    public JwtResponse login(FormLogin request){
+        Users users = authRepository.findByUsername(request.getUsername()).orElseThrow(() -> new RuntimeException("User not found"));
+        if (!passwordEncoder.matches(request.getPassword(), users.getPassword())) {
+            throw new RuntimeException("Invalid password");
+        }
+        String token = jwtUtil.generateToken(users);
+        return JwtResponse.builder()
+                .token(token)
+                .type("Bearer")
+                .expiration(expirationTime)
+                .build();
     }
 }
